49401e6103f1e4c53b04f516d15215cf9d7bd2af
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-08-21 21:49:35 +0900

    fix finalDistanceWeight

--- auto_stabilizer/rtc/AutoStabilizer/LegCoordsGenerator.h
+++ auto_stabilizer/rtc/AutoStabilizer/LegCoordsGenerator.h
@@ -9,7 +9,7 @@ public:
 
   double delayTimeOffset = 0.2; // 0以上. swing期は、remainTime - supportTime - delayTimeOffset後にdstCoordsに到達するようなrectangle軌道を生成し、その軌道にdelayTimeOffset遅れで滑らかに追従するような軌道を生成する
   double touchVel = 0.5; // 0より大きい. 足を下ろすときの速さ(この値より速く下ろす) [m/s]
-  double finalDistanceWeight = 3.0; // 0より大きい. swing期のDOWN_PHASEの速さを、他のPHASEとくらべ何倍遅くするか. 着地時の衝撃をやわらげる
+  double finalDistanceWeight = 1.0; // 0より大きい. swing期のDOWN_PHASEの速さを、他のPHASEとくらべ何倍遅くするか. 大きくすると着地が遅くなって着地衝撃をやわらげる? 大きくするとswingの速度が上がるため、遊脚の力センサにノイズが乗って着地誤検知しやすい
   double goalOffset = -0.05; // [m]. 遊脚軌道生成時に、次に着地する場合、generate frameで鉛直方向に, 目標着地位置に対して加えるオフセット. 0以下. 歩行中は急激に変更されない
 
   int previewStepNum = 2; // 2以上. 着地位置修正アルゴリズムが1 step capturabilityに基づくものであるため、今の一歩より先のstepを予見して重心軌道を生成してしまうと破綻する. そのため、2がよい.
